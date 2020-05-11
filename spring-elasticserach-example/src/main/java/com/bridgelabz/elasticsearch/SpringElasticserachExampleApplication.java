ee87b98cbb58e308d0618cbbf01f8a580476368f
chaithra-harshi <chaithrabn96@gmail.com>
2020-05-11 15:42:36 +0530

    [chaithra]Adding: elasticSearch example in STS

---
+++ spring-elasticserach-example/src/main/java/com/bridgelabz/elasticsearch/SpringElasticserachExampleApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringElasticserachExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringElasticserachExampleApplication.class, args);
+	}
+
+}
